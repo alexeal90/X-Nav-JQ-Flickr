915e7004e31b411d4f471acc18392eb8f224e111
alexeal90 <alexeal90@gmail.com>
2016-04-18 13:46:07 +0200

    Hecho ejercicio JSONP flickr

--- jsonp.js
+++ jsonp.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     function busqueda(search){
         $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=" + search + "&tagmode=any&format=json&jsoncallback=?")
         .done(function(data) {
-            $("#album").remove();
+            $("#album").empty();
             var i = 0;
             $(data.items).each(function (){
                 var fotos = data.items[i].media.m
